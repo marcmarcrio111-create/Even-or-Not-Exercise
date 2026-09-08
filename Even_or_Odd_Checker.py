aa906cf0f6d9587a5bbb219e046ebfc2931e5482
marcmarcrio111-create <thirdyrio111@gmail.com>
2026-09-08 15:31:33 +0800

    Update Even_or_Odd_Checker.py

--- Even_or_Odd_Checker.py
+++ Even_or_Odd_Checker.py
@@ -1,9 +1,9 @@
 #1 Input the number (the variable is called "even_or_not")
-num = float(input("Enter a number: "))
+even_or_not = float(input("Enter a number: "))
 
-if num % 2 == 0:
-  print(f"{num} is an even number.")
+if even_or_not % 2 == 0:
+  print(f"{even_or_not} is an even number.")
 else:
-  print(f"{num} is an odd number.")
+  print(f"{even_or_not} is an odd number.")
 
  
